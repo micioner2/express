3ed1f38f57e40f221a4560a1bcdc9e3434ff3717
micioner2 <mauricio_pella1@hotmail.com>
2019-10-17 19:32:34 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+// const express = require('express');
+import morgan from 'morgan'
+// const morgan = require('morgan');
+
+import cors from 'cors'
+// const cors = require('cors');
+
+import path from 'path'
+
+import mongoose from 'mongoose'
+
+//Conexion a la base de datos
+mongoose.Promise = global.Promise
+const dbUrl = 'mongodb://localhost:27017/dbsistema'
+mongoose.connect(dbUrl, {useCreateIndex:true,useNewUrlParser: true})
+.then(mongoose => console.log('conectando a la BD  en el puerto 27017'))
+.catch(err => console.log(err));
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')))
+app.set('port',process.env.PORT || 3000)
+
+app.listen(app.get('port'),()=>{
+    console.log('servidor se esta ejecutando en el puerto ' + app.get('port'));
+
+})
